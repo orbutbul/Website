79628ba936069ea99e1084469640c9df87472120
orbutbul <butbulor@gmail.com>
2023-12-08 23:17:55 -0500

    refreshed text

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-// function toggleDarkMode() {
-//     const root = document.documentElement;
+console.log("Sorry, dark mode isnt available yet. \nIf you would like to see this website I have the repo linked below!")
+console.log("hhttps://github.com/orbutbul/Website")
 
-//     const currentBackgroundColor = getComputedStyle(root).getPropertyValue('--main-bg-color');
-
-
-//     if (currentBackgroundColor === '#F5EEE4') {
-//         root.style.setProperty('--main-bg-color', '#262626');
-//         root.style.setProperty('--main-primary', '#CAA4CF');
-//         root.style.setProperty('--main-secondary', '#040301');
-//         root.style.setProperty('--main-accent', '#C2B7B9');
-//         root.style.setProperty('--main-text', '#F5EEE4');
-//     } else {
-//         root.style.setProperty('--main-bg-color', '#F5EEE4');
-//         root.style.setProperty('--main-primary', '#CAA4CF');
-//         root.style.setProperty('--main-secondary', 'burlywood');
-//         root.style.setProperty('--main-accent', '#79676A');
-//         root.style.setProperty('--main-text', '#262626');
-//     }
-// }
 function validateInput(inputElement) {
     const inputValue = inputElement.value;
     const regex = /^[a-zA-Z0-9]+$/; // Regular expression for letters and numbers
